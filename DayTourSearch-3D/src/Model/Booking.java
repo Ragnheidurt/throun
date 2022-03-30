a85d461a18116370b3f14cb431f2629ea2d2a044
Valgeir99 <valgeir99@gmail.com>
2022-03-30 10:32:56 +0000

    Bla

--- DayTourSearch-3D/src/Model/Booking.java
+++ DayTourSearch-3D/src/Model/Booking.java
@@ -1,5 +1,37 @@
 package Model;
 
 public class Booking {
-    int Customer;
+    private int customerId;
+    private int dayTripId;
+    private int numberOfGuests;
+
+    public Booking(int customerId, int dayTripId, int numberOfGuests) {
+        this.customerId = customerId;
+        this.dayTripId = dayTripId;
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public int getDayTripId() {
+        return dayTripId;
+    }
+
+    public void setDayTripId(int dayTripId) {
+        this.dayTripId = dayTripId;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
 }
