557c88ac58a0dc989d31c7baad9c6238dce77ca2
Valgeir99 <valgeir99@gmail.com>
2022-03-31 15:06:43 +0000

    Vidmot

--- DayTourSearch-3D/src/Controller/DayTripController.java
+++ DayTourSearch-3D/src/Controller/DayTripController.java
@@ -1,4 +1,15 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+
+import java.time.LocalDate;
+
 public class DayTripController {
+
+
+    @FXML
+    private DatePicker fxDate;
+
+
 }
