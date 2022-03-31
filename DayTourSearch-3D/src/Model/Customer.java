d97e9ae2d958de0d5b9188048e70d459a968a851
sveppikrull <sverrir22@gmail.com>
2022-03-31 13:26:54 +0000

    Sverrir DayTrip

--- DayTourSearch-3D/src/Model/Customer.java
+++ DayTourSearch-3D/src/Model/Customer.java
@@ -1,4 +1,30 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class Customer {
+    private final int customerId;
+    private ArrayList<Booking> bookings;
+    private String password;
+
+    public Customer(int customerId, String password) {
+        this.customerId = customerId;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public ArrayList<Booking> getBookings() {
+        return bookings;
+    }
 }
